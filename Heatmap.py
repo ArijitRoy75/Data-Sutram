141e3564a34dbbad53d8cfa57c3561911314f27f
Arijit Ray <35370469+ArijitRoy75@users.noreply.github.com>
2019-07-14 15:40:01 +0530

    Update Heatmap.py

--- Heatmap.py
+++ Heatmap.py
@@ -1,11 +1,24 @@
-import math
+#defining funcition for list normalisation
+def normalize_list(lis):
+    max_value = max(lis)
+    min_value = min(lis)
+    for i in range(0, len(lis)):
+        lis[i] = (lis[i] - min_value) / (max_value - min_value)
+    return lis
+
+#importing required libraries
 from mpl_toolkits.basemap import Basemap
 import matplotlib.pyplot as plt
 import pandas as pd
+
+#reading the excel sheet 
 coords=pd.read_excel('heatmap_buy_20.xlsx').iloc[1:].values
+#converting the columns of the datframes into lists
 lons = coords[:,0].tolist()
 lats = coords[:,1].tolist()
 weights=coords[:,2].tolist()
+
+#creating a miller basemap object with lower left coordinate as(88.1 East,22.1 North), and upper right coordinate as (88.8 East,22.9 North)
 fig = plt.figure(figsize=(13, 13))
 m = Basemap(projection='mill',
             llcrnrlon=88.1,
@@ -15,11 +28,12 @@ m = Basemap(projection='mill',
             resolution = 'f')
 m.drawmapboundary(fill_color='white')
 m.drawcoastlines(color='black')
-w=[]
-for i in weights:    
-    a=math.sqrt(i)
-    w.append(a)
+
+#normalizing weights
+w=normalize_list(weights)
+
+#creating a scatterplot heatmap
 m.scatter(lons, lats, latlon=True,c=w, cmap='RdBu_r', alpha=0.8)
-plt.colorbar(label="SquareRoot(Weights)")
+plt.colorbar(label="Normalised Weights")
 plt.title(label="Weight Variation Heatmap on Basemap")
 plt.show()
